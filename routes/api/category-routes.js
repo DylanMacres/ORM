4a81bc7b0ade86595610b3922f22e5f5b7313cfd
DylanMacres <Dylan.Macres@du.edu>
2022-10-25 15:11:39 -0600

    added more to apis

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -61,8 +61,18 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try{
+    const data = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.json(data)
+  }catch(err) {
+    res.status(420).json(err)
+  }
 });
 
 module.exports = router;
