a6b005b7d714a1e71a2795d9c814054d6b8bffbf
DylanMacres <Dylan.Macres@du.edu>
2022-10-25 17:47:10 -0600

    fixed my routes and finished the api folder

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,16 +34,45 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const data = await Tag.create(req.body, {
+      include: [{model: Product}],
+    });
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(420).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+router.put('/:id',async (req, res) => {
+  // update a tag's name by its `id` value\
+  try{
+    const data = await Tag.update(
+      { tag_name: req.body.tag_name },
+      {
+        where: { id: req.params.id },
+      }
+    );
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(420).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const data = await Tag.destroy(
+      {
+        where: { id: req.params.id },
+      }
+    );
+    res.status(200).json(data)
+  } catch (error) {
+    res.status(420).json(err)
+  }
 });
 
 module.exports = router;
