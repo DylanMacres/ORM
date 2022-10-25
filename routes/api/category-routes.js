b29443b651f6de54ac08fb8a5fe17fa9d35b671a
DylanMacres <Dylan.Macres@du.edu>
2022-10-25 15:00:10 -0600

    adding to the apis

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,35 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  try{
+    const data = await Category.findAll({
+      include: [{ model: Product }],
+    })
+    res.json(data);
+  }
+  catch(err) {
+    res.status(420).json(err)
+  }
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const data = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}],
+    }); 
+    if(!data) {
+      res.status(404).json({message: "Sorry couldn't find that category!"});
+      return;
+    }
+    res.json(data)
+  }catch(err){
+    res.status(420).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
